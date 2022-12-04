22e2719e4b12124fa576bde07ca40561b7323c90
max ron <spirit@maxrax.de>
2022-12-04 19:18:15 +0100

    Add Day 4 Solution

---
+++ Solutions.Tests/Day_04/UnitTest.cs
@@ -0,0 +1,46 @@
+namespace Solutions.Tests.Day_04;
+
+public class UnitTest
+{
+    [Theory]
+    [MemberData(nameof(GetTestData))]
+    public Task Can_GetMatchCount(string input, int expected)
+    {
+        // Given is MemberData
+
+        int actual = Solutions.Day_04.Solution.GetMatchCount(input);
+
+        // Then
+        Assert.Equal(expected, actual);
+
+
+        return Task.CompletedTask;
+    }
+
+    public static IEnumerable<object[]> GetTestData()
+    {
+        // Case 1 from example
+        var input = """
+        2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8
+        """;
+        var expected = 2;
+
+        yield return new object[] { input, expected };
+
+        // Case 2 from example but modified
+        input = """
+        1-9,2-8
+        3-6,4-5
+        5-8,6-7
+        1-2,2-3
+        """;
+        expected = 3;
+
+        yield return new object[] { input, expected };
+    }
+}
---
+++ Solutions/Day_04/Solution.cs
@@ -0,0 +1,76 @@
+namespace Solutions.Day_04;
+
+public static class Solution
+{
+    public static int GetMatchCount(string input)
+    {
+        var matchCount = 0;
+        var rangesCollection = GetConversions(input);
+
+        foreach (var ranges in rangesCollection)
+            if (IsOneCompletelyWithinOther(ranges))
+                matchCount++;
+
+        return matchCount;
+    }
+
+    private static bool IsOneCompletelyWithinOther(Tuple<List<int>, List<int>> ranges)
+    {
+        var left = Enumerable.Range(ranges.Item1.First(), ranges.Item1.Count());
+        var right = Enumerable.Range(ranges.Item2.First(), ranges.Item2.Count());
+
+        if (GetCountOfWithins(left, right) == right.Count())
+            return true;
+
+        if (GetCountOfWithins(right, left) == left.Count())
+            return true;
+
+        return false;
+    }
+
+    private static int GetCountOfWithins(IEnumerable<int> container, IEnumerable<int> range)
+    {
+        var counter = 0;
+
+        foreach (var item in range)
+            if (container.Contains(item))
+                counter++;
+
+        return counter;
+    }
+
+    private static List<Tuple<List<int>, List<int>>> GetConversions(string input)
+    {
+        var opponentsCollection = new List<Tuple<List<int>, List<int>>>();
+
+        var matrix = input
+            .Split("\n".ToArray())
+                .Select(line => line.Split(',', StringSplitOptions.RemoveEmptyEntries))
+                .ToArray();
+
+        for (var i = 0; i < matrix.Length; i++)
+        {
+            List<int>? left = null;
+            List<int>? right = null;
+
+            var pair = matrix[i];
+
+            while (left is null && right is null)
+            {
+                foreach (var range in pair)
+                {
+                    var start = Convert.ToInt32(Char.GetNumericValue(range.First()));
+                    var count = Convert.ToInt32(Char.GetNumericValue(range.Last()) - (start - 1));
+
+                    var digits = new List<int>(Enumerable.Range(start, count));
+
+                    _ = left is null ? left = digits : right = digits;
+                }
+            }
+
+            opponentsCollection.Add(Tuple.Create<List<int>, List<int>>(left!, right!));
+        }
+
+        return opponentsCollection;
+    }
+}
