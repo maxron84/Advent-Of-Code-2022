77c01573b1bcd47a9f497635bdc1dd816998fbde
max ron <spirit@maxrax.de>
2022-12-07 18:24:18 +0100

    Add Library

---
+++ Solutions/Library.cs
@@ -0,0 +1,18 @@
+namespace Solutions;
+
+public static class MatrixOperator<T>
+{
+    internal static T[] GetMatrixColumn(T[][] matrix, int column)
+    {
+        return Enumerable.Range(0, matrix.GetLength(0))
+            .Select(row => matrix[row][column])
+            .ToArray();
+    }
+
+    internal static T[] GetMatrixRow(T[][] matrix, int row)
+    {
+        return Enumerable.Range(0, matrix.GetLength(1))
+            .Select(column => matrix[row][column])
+            .ToArray();
+    }
+}
