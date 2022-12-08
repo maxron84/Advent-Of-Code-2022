f74977e9f43dfb90f24cb07753d74c522c4eaa32
Max Ron <gt72@maxrax.de>
2022-12-08 15:24:49 +0100

    Update Semantics

--- Solutions.Tests/Day_06/UnitTest.cs
+++ Solutions.Tests/Day_06/UnitTest.cs
@@ -2,5 +2,23 @@ namespace Solutions.Tests.Day_06;
 
 public class UnitTest
 {
+    [Theory]
+    [MemberData(nameof(GetTestData))]
+    public Task TestName()
+    {
+        // Given is MemberData
 
+        // When
+
+        // Then
+
+        return Task.CompletedTask;
+    }
+
+    public static IEnumerable<object[]> GetTestData()
+    {
+        // Case 1 from example
+
+        yield return new object[] { };
+    }
 }
