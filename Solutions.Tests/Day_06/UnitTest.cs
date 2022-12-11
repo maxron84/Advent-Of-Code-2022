121720e3d549a2f48eb828a49cc5a11e85f4c43f
max ron <spirit@maxrax.de>
2022-12-11 16:18:21 +0100

    Add Day 6 Solution

--- Solutions.Tests/Day_06/UnitTest.cs
+++ Solutions.Tests/Day_06/UnitTest.cs
@@ -4,21 +4,83 @@ public class UnitTest
 {
     [Theory]
     [MemberData(nameof(GetTestData))]
-    public Task TestName()
+    public Task Can_GetPositionOfCharacterAfterFirstMarker(string input, int subpartLength, int expected)
     {
         // Given is MemberData
 
         // When
+        var actual = Solutions.Day_06.Solution.GetPositionOfCharacterAfterFirstMarker(input, subpartLength);
 
         // Then
+        Assert.Equal(expected, actual);
 
         return Task.CompletedTask;
     }
 
     public static IEnumerable<object[]> GetTestData()
     {
-        // Case 1 from example
+        var subpartLength = 4;
 
-        yield return new object[] { };
+        // Case 1 from example 1
+        var input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        var expected = 7;
+
+        yield return new object[] { input, subpartLength, expected };
+
+        // Case 2 from example 2
+        input = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+        expected = 5;
+
+        yield return new object[] { input, subpartLength, expected };
+
+        // Case 3 from example 3
+        input = "nppdvjthqldpwncqszvftbrmjlhg";
+        expected = 6;
+
+        yield return new object[] { input, subpartLength, expected };
+
+        // Case 4 from example 4
+        input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+        expected = 10;
+
+        yield return new object[] { input, subpartLength, expected };
+
+        // Case 5 from example 5
+        input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+        expected = 11;
+
+        yield return new object[] { input, subpartLength, expected };
+    }
+
+    [Theory]
+    [MemberData(nameof(GetTestData_FromFiles))]
+    public Task Can_GetPositionOfCharacterAfterFirstMarker_FromFiles(string input, int subpartLength)
+    {
+        // Given is MemberData
+
+        // When
+        var actual = Solutions.Day_06.Solution.GetPositionOfCharacterAfterFirstMarker(input, subpartLength);
+
+        // Then
+        // Save returnvalues for comparison with dataprovider here...
+        // 1: 1920
+        // 2: 2334
+
+        return Task.CompletedTask;
+    }
+
+    public static IEnumerable<object[]> GetTestData_FromFiles()
+    {
+        var input = File.ReadAllText(@".\Day_06\Input.txt");
+
+        // Case 1 from part one of puzzle
+        var subpartLength = 4;
+
+        yield return new object[] { input, subpartLength };
+
+        // Case 2 from part two of puzzle
+        subpartLength = 14;
+
+        yield return new object[] { input, subpartLength };
     }
 }
--- Solutions/Day_06/Solution.cs
+++ Solutions/Day_06/Solution.cs
@@ -2,5 +2,25 @@ namespace Solutions.Day_06;
 
 public static class Solution
 {
-    
+    private static StringBuilder _stringBuilder = new();
+
+    public static int GetPositionOfCharacterAfterFirstMarker(string input, int subpartLength)
+    {
+        var index = subpartLength - 1;
+
+        for (var i = index; i < input.Length - 1; i++)
+        {
+            _stringBuilder.Clear();
+
+            for (var j = i - index; j <= i; j++)
+                _stringBuilder.Append(input[j]);
+
+            var substringNegative = _stringBuilder.ToString();
+
+            if (substringNegative.Distinct().Count() == substringNegative.Length)
+                return i + 1;
+        }
+
+        return -1;
+    }
 }
